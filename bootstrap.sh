53754e7ac16ac27d37c0214868ed32eab3bb9b0c
onesmallskipforman <sgonzalez@g.hmc.edu>
2020-04-06 15:05:08 -0400

    Modified Boostrap Script to Handle Separate Dotfile Repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,57 +1,36 @@
 #!/bin/zsh
 
-# git pull origin master;
-
-# git clean -dfX
+function bootstrap() {
+  mkdir -p "$1"
+  git -C "$1" init
+  git -C "$1" remote add origin "$2"
+  git -C "$1" pull origin master
+  # # git -C "$1" clean -fdX
+}
 
 function doIt() {
 
-  # # download files from github
-  # REPO="https://github.com/onesmallskipforman/Backup.git"
-  # BACKUP="$XDG_DATA_HOME"
-  # mkdir -p "$BACKUP"
+  # boostrap scripts
+  boostrap $PWD "https://github.com/onesmallskipforman/bootstrap.git"
 
-  # git -C "$BACKUP" init
-  # git -C "$BACKUP" remote add origin "$REPO"
-  # git -C "$BACKUP" pull origin master
-  # git -C "$BACKUP" clean -fdX
+  # public dotfiles
+  PUBLIC="$PWD/Filesystem"
+  boostrap "$PUBLIC" "https://github.com/onesmallskipforman/dotfiles.git"
 
-  # remove old files
-  rm -rf "$HOME/.config"
-  rm -rf "$HOME/.local"
-  rm -rf "$HOME/.zshenv"
+  # private dotfiles
+  PRIVATE="$PUBLIC/.local/share"
+  boostrap "$PRIVATE" "https://github.com/onesmallskipforman/userdata.git"
 
-  # symlink (or copy) .config, .local and .zshrc
-  ln -sf "$PWD/Filesystem/.config" "$HOME"
-  ln -sf "$PWD/Filesystem/.local"  "$HOME"
-  ln -sf "$PWD/Filesystem/.zshenv" "$HOME"
+  # remove old files, then symlink (or copy)
+  rm -rf "$HOME"/{.config,.local,.zshenv}
+  ln -sf "$PWD/Filesystem"/* "$HOME"
+  # cp -r "$PWD/.config"/* "$HOME"
 
-  # cp -r "$PWD/.config" "$HOME"
-  # cp -r "$PWD/.local"  "$HOME"
-  # cp -r "$PWD/.zshrc"  "$HOME"
-
-  # move application support
-
-  # rm -rf ~/.dotfiles
-  # rsync -a --delete --delete-excluded --info=progress2 \
-  #   --exclude ".git/" \
-  #   --exclude ".gitignore" \
-  #   --exclude ".DS_Store" \
-  #   --exclude "Experimental/" \
-  #   --exclude "README.md" \
-  #   --exclude "todo.txt" \
-  #   --exclude ".zshrc" \
-  #   . ~/.dotfiles/;
-
-  # rsync -a --delete --info=progress2 \
-  #   --no-perms .zshrc ~/.zshrc;
 }
 
 if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
   doIt;
 else
-  # read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  # echo "";
   printf '%s ' "This may overwrite existing files in your home directory. Are you sure? (y/n)";
   read REPLY;
 
