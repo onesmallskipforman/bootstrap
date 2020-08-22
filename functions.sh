8d94d8019ac67be50a6f49afe085222019322ddc
onesmallskipforman <skipper.gonzalez@icloud.com>
2020-08-22 13:56:26 -0400

    Separate apt items into keys, ppas, and packages

--- functions.sh
+++ functions.sh
@@ -40,18 +40,11 @@ function prep_osx() {
   sudo spctl --master-disable           # allow apps downloaded from anywhere
 
   # install command line tools
-  xcode-select -p &>/dev/null || (
-    echo "Installing Command Line Tools..." &&
-    xcode-select --install
-  )
-  echo "OS Prep Complete."
+  xcode-select -p &>/dev/null || xcode-select --install
 }
 
 function prep_ubuntu() {
   sudo apt-get install -y git gcc
-
-  # 32-bit architechture for modelsim
-  sudo dpkg --add-architecture i386
 }
 
 #===============================================================================
@@ -83,19 +76,9 @@ function cargo_install() {
   bigprint "Installing Cargo Packages"
   which rustup &>/dev/null || (
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    rustup override set stable
   )
-  rustup update stable
-  source $HOME/.cargo/env
+  rustup override set stable; rustup update stable; source $CARGO_HOME/env
   cat "Packages/cargo_$OS.txt" | xargs -n1 cargo install --git
-}
-
-function goget_install() {
-  bigprint "Installing Go Packages"
-  which go &>/dev/null || (
-    wget -qO - https://dl.google.com/go/go1.11.linux-amd64.tar.gz | tar -C /usr/local -xzf -
-  )
-  cat "Packages/go_$OS.txt" | xargs -n1
 }
 
 function git_install() {
@@ -107,50 +90,25 @@ function git_install() {
   echo "Repo Cloning Complete."
 }
 
-function pkg_install_ubuntu() {
-  # Install Apt Package Repos and Packages
+function pkg_install() {
   bigprint "Installing Packages."
-  sudo apt-get update -y && sudo apt-get dist-upgrade -y
+  pkg_install_$OS
+  echo "Package Install Complete."
+}
 
-  # import keys
-  grep '^key' "Packages/aptfile"  \
-    | sed 's/^[^"]*"//; s/".*//' \
-    | while read key; do wget -qO - $key | sudo apt-key add -; done
-  sudo apt-get update -y && sudo apt-get dist-upgrade -y
-
-  # add repos
-  grep '^repo' "Packages/aptfile"  \
-    | sed 's/^[^"]*"//; s/".*//' \
-    | xargs -n1 -I{} sudo add-apt-repository -y "{}"
-  sudo apt-get update -y && sudo apt-get dist-upgrade -y
-
-  # install apt packages
-  grep '^apt' "Packages/aptfile" \
-    | sed 's/^[^"]*"//; s/".*//' \
-    | xargs sudo apt-get -y -o Dpkg::Options::=--force-confdef install
+function pkg_install_ubuntu() {
+  # install apt keys, repos, and packages, then cleanup
+  xargs -a key.txt sudo apt-key adv --fetch-keys
+  xargs -a ppa.txt sudo add-apt-repository -y install; sudo apt-get update
+  xargs -a apt.txt sudo apt-get -y install
   sudo apt-get update -y --fix-missing && sudo apt-get dist-upgrade -y && sudo apt-get -y autoremove
-
-  # # alternative for deb files
-  # grep '^deb' "Packages/aptfile"  \
-  #   | while IFS=, read url list; do
-  #       url=$(sed 's/^[^"]*"//; s/".*//' <<< $url)
-  #       list=$(sed 's/^[^"]*"//; s/".*//' <<< $list)
-  #       echo "deb $url" | sudo tee /etc/apt/sources.list.d/$list
-  #     done
-  # sudo apt-get update -y && sudo apt-get dist-upgrade -y
-
 }
 
 function pkg_install_osx() {
   # homebrew-managed installations
-  bigprint "Installing Packages."
-  # [[ ! "$OSTYPE" = "darwin"* ]] && echo "OS Config Complete." && return
-  which brew &>/dev/null || (           # install homebrew if missing
-    echo "Installing Homebrew..." &&
+  which brew &>/dev/null || \
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  )
   brew bundle -v --no-lock "Packages/brewfile"
-  echo "Brew Setup Complete."
 }
 
 #===============================================================================
@@ -158,7 +116,7 @@ function pkg_install_osx() {
 #===============================================================================
 
 function config() {
-  bigprint "Runnung Post-Install Configs"
+  bigprint "Runnung Miscellaneous Post-Package Installs and Configs"
 
   # default shell to zsh
   sudo chsh -s /bin/zsh
@@ -172,6 +130,10 @@ function config() {
 function config_ubuntu() {
   # Set computer name
   hostnamectl set-hostname SkippersMPB
+  quartus_install
+  light_install
+  sudo rosdep init && rosdep update
+  ~/.local/src/nerd-fonts/install.sh Hack
 }
 
 function config_osx() {
@@ -191,6 +153,15 @@ function config_osx() {
   # configure yabai wm with scripting addition
   [ yabai --check-sa ] || sudo yabai --install-sa
 
+  # set symlinks for zathura, minecraft, spotify preferences
+  mkdir -p $(brew --prefix zathura)/lib/zathura
+  ln -sf $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+  ln -sf "$HOME/.config/minecraft/options.txt" "$HOME/Library/ApplicationSupport/Minecraft/options.txt"
+  ln -sf "$HOME/.local/share/spotify/prefs"    "$HOME/Library/ApplicationSupport/Spotify/prefs"
+
+  # configure xdg shell completion
+  $(brew --prefix)/opt/fzf/install --xdg
+
   echo "OS Config Complete. Restart Required"
 }
 
@@ -198,29 +169,11 @@ function config_osx() {
 # MISCELLANEOUS
 #===============================================================================
 
-function misc() {
-  bigprint "Running Miscellaneous Installs and Configs"
-  misc_$OS
-  echo "Misc. operations complete."
-}
-
-function misc_ubuntu() {
-  quartus_install
-  light_install
-  ros_config
-  ~/.local/src/nerd-fonts/install.sh Hack
-}
-
-function misc_osx() {
-  # math_install
-  mkdir -p $(brew --prefix zathura)/lib/zathura
-  ln -sf $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
-  ln -sf "$HOME/.config/minecraft/options.txt" "$HOME/Library/ApplicationSupport/Minecraft/options.txt"
-  ln -sf "$HOME/.local/share/spotify/prefs"    "$HOME/Library/ApplicationSupport/Spotify/prefs"
-  ln -sfn "$HOME/.config/Code/User"            "$HOME/Library/ApplicationSupport/Code/User"
-}
-
 function quartus_install() {
+
+  # 32-bit architechture for modelsim
+  sudo dpkg --add-architecture i386
+
   ADIR="$HOME/.local/share/altera"
 
   # Unzip tar
@@ -228,15 +181,15 @@ function quartus_install() {
   tar -C $ADIR/Install -xvf $ADIR/Quartus-web-15.0.0.145-linux.tar
 
   # install software
-  sudo $ADIR/Install/setup.sh \
-    --mode unattended \
-    --unattendedmodeui minimalWithDialogs \
-    --installdir /opt/altera/15.0
+  sudo $ADIR/Install/setup.sh --mode unattended \
+    --unattendedmodeui minimalWithDialogs --installdir /opt/altera/15.0
 
   # set up permissions for usb blaster
-  echo '# For Altera USB-Blaster permissions. \
-  SUBSYSTEM=="usb",\
-  ENV{DEVTYPE}=="usb_device",\ATTR{idVendor}=="09fb",\ATTR{idProduct}=="6001",\MODE="0666",\NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}",\RUN+="/bin/chmod 0666 %c"'| sudo tee /etc/udev/rules.d/51-usbblaster.rules > /dev/null
+  echo '# For Altera USB-Blaster permissions. \SUBSYSTEM=="usb",\
+  ENV{DEVTYPE}=="usb_device",\ATTR{idVendor}=="09fb",\ATTR{idProduct}=="6001",\
+  MODE="0666",\NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}",\
+  RUN+="/bin/chmod 0666 %c"'| \
+  sudo tee /etc/udev/rules.d/51-usbblaster.rules > /dev/null
 }
 
 function light_install() {
@@ -245,11 +198,6 @@ function light_install() {
   ./configure && make
   sudo make install
   cd ~-
-}
-
-function ros_config() {
-  sudo rosdep init
-  rosdep update
 }
 
 function matlab_install() {
